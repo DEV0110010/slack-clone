89f6254e7047cd2428787258b2e36d24b857e70d
DEV0110010 <newera.deepanshu@gmail.com>
2025-08-27 17:12:04 +0530

    tailwind and custom hooks

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
 import { UserButton } from '@clerk/clerk-react'
 import React from 'react'
+import { useStreamChat } from '../hooks/useStreamchat';
 
 const HomePage = () => {
+   const { chatClient, error, isLoading } = useStreamChat();
   return (
     <div>
       <UserButton/>
