d7ab589b51b4464ec561c4641a5f76793212bfc8
DEV0110010 <newera.deepanshu@gmail.com>
2025-08-31 19:58:56 +0530

    final setup

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,11 +2,18 @@
 
 import axios from 'axios';
 
-const BASE_URL = import.meta.env.MODE === 'development'
-? "http://localhost:5001/api" : "https://slack-clone-nine-navy.vercel.app/api";
+// const BASE_URL = import.meta.env.MODE === 'development'
+// ? "http://localhost:5001/api" : "https://slack-clone-nine-navy.vercel.app/api";
 
+
+// export const axiosInstance = axios.create({
+//     baseURL: BASE_URL,
+//     withCredentials: true,
+// })
+
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const axiosInstance = axios.create({
-    baseURL: BASE_URL,
-    withCredentials: true,
-})
+  baseURL: BASE_URL,
+  withCredentials: true,
+});
